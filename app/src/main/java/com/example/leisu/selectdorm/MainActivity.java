135fd4b70f22ac795418361f5ae56c96d02f34e0
ruanfangqi <ruanfq@pku.edu.cn>
2017-10-27 19:54:35 +0800

    init project

---
+++ app/src/main/java/com/example/leisu/selectdorm/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.leisu.selectdorm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by leisu on 2017/10/27.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dorm_info);
+    }
+}
