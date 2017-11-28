f99f69383797181a3249ef57a68d7ef82983d653
ruanfangqi <ruanfq@pku.edu.cn>
2017-11-29 00:10:53 +0800

    display

--- app/src/main/java/com/example/leisu/selectdorm/MainActivity.java
+++ app/src/main/java/com/example/leisu/selectdorm/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.leisu.selectdorm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by leisu on 2017/10/27.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private ImageView sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dorm_info);
+
+        sign = (ImageView)findViewById(R.id.wel_btn_sign);
+        sign.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.wel_btn_sign) {
+
+        }
     }
 }
